701fd94ebfbdc18ce7fc4d70e0d8868b1fc42d91
Erik Winge <erik.winge@ub.uio.no>
2020-05-19 10:42:57 +0200

    Disable Cambridge/Springer RSS for now

--- newsletter.py
+++ newsletter.py
@@ -260,11 +260,11 @@ def fetch_all():
     heading("Nye e-bøker", level="h2")
     fetch_books("https://ub-tilvekst.uio.no/lists/72.json?days=%d" % options.days)
 
-    heading("Nye e-bøker fra Cambridge", level="h2")
-    fetch_feeds(["https://www.cambridge.org/core/rss/subject/id/7C9FB6788DD8D7E6696263BC774F4D5B"], item_count=-1, filter_title='[Book]')
+#     heading("Nye e-bøker fra Cambridge", level="h2")
+#     fetch_feeds(["https://www.cambridge.org/core/rss/subject/id/7C9FB6788DD8D7E6696263BC774F4D5B"], item_count=-1, filter_title='[Book]')
 
-    heading("Nye e-bøker fra Springer", level="h2")
-    fetch_feeds(["https://link.springer.com/search.rss?facet-discipline=%22Law%22&showAll=false&facet-language=%22En%22&facet-content-type=%22Book%22"], item_count=-1)
+#     heading("Nye e-bøker fra Springer", level="h2")
+#     fetch_feeds(["https://link.springer.com/search.rss?facet-discipline=%22Law%22&showAll=false&facet-language=%22En%22&facet-content-type=%22Book%22"], item_count=-1)
 
     heading("Nye trykte bøker", level="h2")
     fetch_books("https://ub-tilvekst.uio.no/lists/68.json?days=%d" % options.days)
